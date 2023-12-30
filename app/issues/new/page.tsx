0b5d4ca0a7f2990eff4f1c4c91ef6a97c1ebf38f
kemal <kadzhimambetov@dpkapp.ru>
2023-12-30 22:54:26 +0300

    fix error ssr

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,7 +4,8 @@ import "easymde/dist/easymde.min.css";
 import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import React, { useState } from "react";
 
-import SimpleMDE from "react-simplemde-editor";
+// import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -14,6 +15,10 @@ import { z } from "zod";
 import { createIssueSchema } from "@/app/schemas/createIssueSchema";
 import { ErrorMessage } from "@/app/components/ErrorMessage";
 import { Spinner } from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
